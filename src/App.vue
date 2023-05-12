68649865366a85fb46630359a7cc342ac242ea4c
what-is-me <2454787428@qq.com>
2023-05-12 10:56:44 +0800

    抄来的css改了改

--- src/App.vue
+++ src/App.vue
@@ -23,14 +23,14 @@
   display: block;
   position: absolute;
   left: 0;
-  top: 60px;
+  top: 0;
   bottom: 0;
 }
 .el-main {
   position: absolute;
   left: 300px;
   right: 0;
-  top: 60px;
+  top: 0;
   bottom: 0;
   overflow-y: scroll;
 }
